367b0b90b1a2073eace2b8a6d22195f8e08489f7
btsarmiento <btsarmiento@up.edu.ph>
2024-08-21 15:06:13 +0800

    hello1

--- problem1.py
+++ problem1.py
@@ -1,3 +1,5 @@
+#defining the multiples of 3 and 5 that is below 100
 multiples = [x for x in range(1, 1000) if x % 3 == 0 or x % 5 == 0]
+#getting the sum of these multiples
 total = sum(multiples)
 print(total)
